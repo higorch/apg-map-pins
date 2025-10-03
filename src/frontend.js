2a362ba6b8ee459e50508920adee47942abaed5b
Higor Ch. <higorchristianfe@gmail.com>
2025-10-03 13:48:08 -0300

    maps

--- src/frontend.js
+++ src/frontend.js
@@ -2,4 +2,4 @@ import { setOptions, importLibrary } from "@googlemaps/js-api-loader";
 
 setOptions({ key: "AIzaSyCYnLD0MCRlcATwL-I10i8LpjrYNbXKEM4" });
 
-const { Map } = await importLibrary("maps")
+const { Map } = await importLibrary("maps");
